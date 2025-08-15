1dd2841639a80991584b0eed88d41e8473a5be7f
Skye Purchase <skyepurchase@gmail.com>
2025-08-15 16:41:27 +0100

    Wrap skel as well

---
+++ public/cgi-bin/skel_lib.py
@@ -0,0 +1,107 @@
+import requests
+from xml.etree import ElementTree
+
+from requests.exceptions import RequestException
+from requests.models import Response
+
+from typing import Dict, Tuple, Union
+
+
+def get_source(
+    url: str,
+    HttpResponse
+) -> Union[Response, RequestException]:
+    try:
+        return requests.get(url)
+    except RequestException:
+        raise HttpResponse(503, "Unable to retrieve data")
+
+
+def get_data(
+    url: str,
+    HttpResponse
+) -> Union[Dict, None]:
+    response = get_source(url, HttpResponse)
+
+    if isinstance(response, RequestException):
+        raise HttpResponse(503, "Invalid response from weather server")
+    else:
+        tree = ElementTree.fromstring(response.content)
+
+        channel: Union[ElementTree.Element, None] = tree.find('channel')
+        if channel is None:
+            raise HttpResponse(503, "Cannot find 'channel' in weather server response")
+
+        item: Union[ElementTree.Element, None] = channel.find('item')
+        if item is None:
+            raise HttpResponse(503, "Cannot find 'item' in weather server response")
+
+        data: Union[ElementTree.Element, None] = item.find('description')
+        if data is None or data.text is None:
+            raise HttpResponse(503, "Not weather data returned")
+
+        table = ElementTree.XML(data.text)
+        rows = iter(table)
+        data_table = {}
+        for row in rows:
+            data_table[row[0].text] = row[1].text
+
+        return data_table
+
+
+def get_input(params: dict) -> Tuple[float, float, float]:
+    minTemp = float(params["min_temp"])
+    midTemp = float(params["mid_temp"])
+    maxTemp = float(params["max_temp"])
+    return (minTemp, midTemp, maxTemp)
+
+
+def convert(temperature: float, params: dict):
+    minT, midT, maxT = get_input(params)
+    grad = 1 / 18.9
+    intercept = -1
+
+    if temperature > midT:
+        grad = 1 / (maxT - midT)
+        intercept = -midT * grad
+    else:
+        grad = 1 / (midT - minT)
+        intercept = (-minT * grad) - 1
+
+    T = (grad * temperature) + intercept
+    return T
+
+
+def run(params, HttpResponse):
+    table = get_data(
+        "https://www.cl.cam.ac.uk/weather/rss.xml",
+        HttpResponse
+    )
+    minT, midT, maxT = get_input(params)
+
+    value = convert(
+        float(table['Temp'][:-2]),
+        params
+    ) if table is not None else 0
+
+    # HTML response
+    with open("templates/skel/template.html") as template:
+        html = template.read()
+
+    values = {
+        "termperature": str(round(value, 2) * 1000),
+        "minT": str(minT),
+        "midT": str(midT),
+        "maxT": str(maxT)
+    }
+
+    print("Content-Type: text/html")
+    print("Status: 200\n")
+
+    def format_html(html: str, replacements: dict) -> str:
+        for key, value in replacements.items():
+            html = html.replace(f"[{key}]", value)
+
+        return html
+
+    print(format_html(html, values))
--- public/cgi-bin/skels.py
+++ public/cgi-bin/skels.py
@@ -1,96 +1,15 @@
 #!/usr/bin/python3
-import cgi
-import requests
-from typing import Dict, Tuple, Union
-from xml.etree import ElementTree
-from requests.exceptions import RequestException
-from requests.models import Response
+# Wrapper for the newsletter main method
 
-from utils import format_html
+from wrap import wrap # Safe import
 
+def run():
+    # All unsafe code that will now be caught
+    from http_lib import params, HttpResponse
+    from skel_lib import run as run_skel
 
-def get_source(url: str) -> Union[Response, RequestException]:
-    try:
-        return requests.get(url)
+    PARAMETERS = params("GET")
 
-    except requests.exceptions.RequestException as e:
-        return e
+    run_skel(PARAMETERS, HttpResponse)
 
-
-def get_data(url: str) -> Union[Dict, None]:
-    response:Union[Response, RequestException] = get_source(url)
-
-    if isinstance(response, RequestException):
-        print("Invalid response")
-        print(response)
-    else:
-        tree = ElementTree.fromstring(response.content)
-
-        channel: Union[ElementTree.Element, None] = tree.find('channel')
-        if channel is None:
-            print("Cannot find 'channel'")
-            return
-
-        item: Union[ElementTree.Element, None] = channel.find('item')
-        if item is None:
-            print("Cannot find 'item'")
-            return
-
-        data: Union[ElementTree.Element, None] = item.find('description')
-        if data is None or data.text is None:
-            print("Cannot find 'data' or related data")
-            return
-
-        table = ElementTree.XML(data.text)
-        rows = iter(table)
-        data_table = {}
-        for row in rows:
-            data_table[row[0].text] = row[1].text
-
-        return data_table
-
-
-def get_input() -> Tuple[float, float, float]:
-    form = cgi.FieldStorage()
-    minTemp = float(form.getvalue("min_temp"))
-    midTemp = float(form.getvalue("mid_temp"))
-    maxTemp = float(form.getvalue("max_temp"))
-    return (minTemp, midTemp, maxTemp)
-
-
-def convert(temperature):
-    minT, midT, maxT = get_input()
-    grad = 1 / 18.9
-    intercept = -1
-
-    if temperature > midT:
-        grad = 1 / (maxT - midT)
-        intercept = -midT * grad
-    else:
-        grad = 1 / (midT - minT)
-        intercept = (-minT * grad) - 1
-
-    T = (grad * temperature) + intercept
-    return T
-
-
-print("Content-Type: text/html")
-print("Status: 200\n")
-
-table = get_data("https://www.cl.cam.ac.uk/weather/rss.xml")
-minT, midT, maxT = get_input()
-
-value = convert(float(table['Temp'][:-2])) if table is not None else 0
-
-# HTML response
-with open("templates/skel/template.html") as template:
-    html = template.read()
-
-values = {
-    "termperature": str(round(value, 2) * 1000),
-    "minT": str(minT),
-    "midT": str(midT),
-    "maxT": str(maxT)
-}
-
-print(format_html(html, values))
+wrap(run)
