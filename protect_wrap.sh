68b1a47c8f37df3936f4f0431cedcafce1544660
Skye Purchase <s.purchase25@imperial.ac.uk>
2026-01-23 17:43:14 +0000

    Use separate protection script

---
+++ protect_wrap.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+if git rev-parse --verify HEAD >/dev/null 2>&1
+then
+  against=HEAD
+else
+  # Initial commit: diff against an empty tree object
+  against=4b825dc642cb6eb9a060e54bf8d69288fbee4904
+fi
+
+exec 1>&2
+
+if git diff --cached --name-only $against |
+   grep --quiet --line-regexp --fixed-strings "public/cgi-bin/wrap.py"
+then
+  echo Commit would modify one or more files that must not change.
+  exit 1
+else
+  exit 0
+fi
